3f1db5e17be2f0a4ae0ecd9c92d1ad3bdfe69914
Christopher Polito <chris@mwclass.io>
2023-03-19 16:25:07 -0400

    fix mobile views

--- src/app/components/header/Header.js
+++ src/app/components/header/Header.js
@@ -3,17 +3,22 @@ import NavBtn from '../navbtn/Navbtn';
 import ButtonGroup from '../common/ButtonGroup';
 import * as u from '../../services/utility.service';
 
-var pageTitle = 'unnecessary theories';
+const pageTitle = 'unnecessary theories';
+
+const showDisplayButtons = false;
 
 const Header = (props) => {
   const { state, img, title, button } = props;
+  const isMobile = u.checkMobile();
   return (
     <div className="relative width">
       {state === 'home' ? (
         <div className="relative width height-600 raised cutoff">
           <div className="absolute width height" id="splash">
             <img
-              className="absolute width height-auto opacity70"
+              className={`absolute ${
+                isMobile ? 'width-auto height' : 'width height-auto'
+              } opacity70`}
               src="img/evolution_chalk.jpg"
               id="banner"
             />
@@ -29,30 +34,48 @@ const Header = (props) => {
 
           <div className="absolute width60 height-200 bottom-50 left-minus-20">
             <div className="absolute width height rounded10 black-back opacity70 border-white"></div>
-            <div className="absolute width80 height80 center beige font-70 dearjoe">
+            <div
+              className={`absolute width80 height80 center beige ${
+                isMobile ? 'font-50' : 'font-70'
+              } dearjoe`}
+            >
               <div className="absolute center">{pageTitle}</div>
             </div>
           </div>
 
-          <div className="absolute width20 height-100 bottom-0 right0">
-            <ButtonGroup
-              isActive={button.isActive}
-              change={button.change}
-              styles={button.styles}
-            />
-          </div>
+          {showDisplayButtons ? (
+            <div
+              className={`absolute ${
+                isMobile ? 'width40' : 'width20'
+              } height-100 bottom-0 right0`}
+            >
+              <ButtonGroup
+                isActive={button.isActive}
+                change={button.change}
+                styles={button.styles}
+              />
+            </div>
+          ) : (
+            <></>
+          )}
         </div>
       ) : (
         <div className="relative width height-600 raised cutoff">
           <div className="absolute width height" id="splash">
             <img
-              className="absolute width height-auto"
+              className={`absolute ${
+                isMobile ? 'width-auto height' : 'width height-auto'
+              }`}
               src={`/${img}.jpg`}
               id="banner"
             />
           </div>
 
-          <div className="absolute width20 height-100 bottom-400 left-minus-20">
+          <div
+            className={`absolute ${
+              isMobile ? 'width40' : 'width20'
+            } height-100 bottom-400 left-minus-20`}
+          >
             <div className="absolute width height rounded10 black-back opacity70 border-white"></div>
             <NavBtn class="center white font-20" name="home" to="/" />
           </div>
