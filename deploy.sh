e49c926434230c0d61146646a9f709b095386e8e
Christopher Polito <chris@mwclass.io>
2025-03-21 11:01:37 -0400

    add fish

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,17 @@ echo "beginning build process"
 
 ENV=$1
 
+if [ $ENV == "dev" ]; then
+    DIST=E2GRDGFBQ38J86
+else
+    DIST=E3HHE4HP52X0W0
+fi
+
 rm -rf build/*
 
 env-cmd -f .env.$ENV npm run build
 aws s3 sync ./build s3://$ENV-blog-infra-frontend
+
+aws cloudfront create-invalidation --distribution-id $DIST --paths "/*"
 
 echo "done"
